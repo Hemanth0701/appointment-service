b61cb9ac8e21edf81bdff87c805a67b6846a5899
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-07-03 16:53:11 +0530

    intial commit

---
+++ src/main/java/com/tenjiku/appointment_service/AppointmentServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tenjiku.appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentServiceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenjiku/appointment_service/AppointmentServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenjiku.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
